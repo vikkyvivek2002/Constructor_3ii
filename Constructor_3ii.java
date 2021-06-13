af1966681282346c6496296ad6ee18a247c55bb4
vikkyvivek2002 <84318895+vikkyvivek2002@users.noreply.github.com>
2021-06-13 23:21:47 +0530

    Add files via upload

---
+++ Constructor_3ii.java
@@ -0,0 +1,19 @@
+class Sample
+{
+Sample()
+{
+System.out.println("This is constructor");
+System.out.println("and this can be accessed through main class");
+System.out.println("using MAIN method ");
+}
+}
+
+class Constructor_3ii
+{
+public static void main(String[] args)
+{
+Sample obj =new Sample();
+
+
+}
+}
